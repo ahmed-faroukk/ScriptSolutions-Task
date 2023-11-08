221a826fdf1ad55e6c61c5760888c24d74cbd6e0
farouk <ahmed.farouk.334455@gmail.com>
2023-11-08 17:29:00 +0200

    Application Base Arch mvvm With Clean Arch + setup dependencies

---
+++ app/src/main/java/com/cyberdunkers/script_solution_task/HiltAndroidApp.kt
@@ -0,0 +1,8 @@
+package com.cyberdunkers.script_solution_task
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class HiltAndroidApp : Application() {
+}
---
+++ app/src/main/java/com/cyberdunkers/script_solution_task/presentation/home/MainScreen.kt
@@ -0,0 +1,8 @@
+package com.cyberdunkers.script_solution_task.presentation.home
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun MainScreen(){
+
+}
